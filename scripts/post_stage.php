eca3e7f5b5d7aec49b474111299a5acbafdc99ca
royganor <roy@zend.com>
2011-12-06 21:34:59 +0200

    Change-Id: I51f13ef06ad8a95c748f805b4b68b8dfb063f86d

--- scripts/post_stage.php
+++ scripts/post_stage.php
@@ -1,9 +1,11 @@
 <?php
-/* The script post_stage.php will be executed after the staging process ends. This will allow
- * users to perform some actions on the source tree or server before an attempt to
- * activate the app is made. For example, this will allow creating a new DB schema
- * and modifying some file or directory permissions on staged source files
- * The following environment variables are accessable to the script:
+/*
+ * The script post_stage.php will be executed after the staging process ends.
+ * This will allow users to perform some actions on the source tree or server
+ * before an attempt to activate the app is made. For example, this will allow
+ * creating a new DB schema and modifying some file or directory permissions on
+ * staged source files The following environment variables are accessable to the
+ * script:
  */
 require_once 'deploy_helpers.php';
 
@@ -15,27 +17,22 @@ $appLocation = getApplicationLocation ();
 $ini_file = $appLocation . '/app/config/parameters.ini';
 $ini_array = parse_ini_file ( $ini_file, true );
 
-$ini_array ['parameters']['database_host'] = str_replace ( '<db-host>', getDbHost(), $ini_array ['parameters']['database_host'] );
-$ini_array ['parameters']['database_name'] = str_replace ( '<db-name>', getDbName(), $ini_array ['parameters']['database_name'] );
-$ini_array ['parameters']['database_user'] = str_replace ( '<username>', getDbUser(), $ini_array ['parameters']['database_user'] );
-$ini_array ['parameters']['database_password'] = str_replace ( '<password>', getDbPassword(), $ini_array ['parameters']['database_password'] );
-$ini_array ['parameters']['secret'] = str_replace ( '<secret-code>', getDbPassword(), $ini_array ['parameters']['secret'] );
+$ini_array ['parameters'] ['database_host'] = str_replace ( '<db-host>', getDbHost (), $ini_array ['parameters'] ['database_host'] );
+$ini_array ['parameters'] ['database_name'] = str_replace ( '<db-name>', getDbName (), $ini_array ['parameters'] ['database_name'] );
+$ini_array ['parameters'] ['database_user'] = str_replace ( '<username>', getDbUser (), $ini_array ['parameters'] ['database_user'] );
+$ini_array ['parameters'] ['database_password'] = str_replace ( '<password>', getDbPassword (), $ini_array ['parameters'] ['database_password'] );
+$ini_array ['parameters'] ['secret'] = str_replace ( '<secret-code>', getDbPassword (), $ini_array ['parameters'] ['secret'] );
 write_ini_file ( $ini_array, $ini_file, true );
 
 /*
  * modify htaccess
  */
 $htaccess_file = $appLocation . '/web/.htaccess';
-$explode = explode('/', $appLocation);
-$appname = $explode[sizeof($explode) -2]; 
-$content = file_get_contents($htaccess_file);
-$content = str_replace('<application-name>', $appname, $content);
-file_put_contents($htaccess_file, $content);
-file_put_contents($appLocation . '/web/log', var_export($appname, true) . 
-		' ' . var_export($explode, true) .
-		$htaccess_file .  
-		' ' . $content);
-
+$explode = explode ('/', $appLocation);
+$appname = $explode [sizeof ($explode) - 2];
+$content = file_get_contents ($htaccess_file);
+$content = str_replace ('<application-name>', $appname, $content);
+file_put_contents ($htaccess_file, $content);
 
 /*
  * chmod cache and logs
